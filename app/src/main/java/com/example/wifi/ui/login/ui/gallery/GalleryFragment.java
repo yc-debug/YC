7ade7ee73f69ef479c8d05effb093a7a94fe1c08
MooRoakee <1422323955@qq.com>
2020-07-03 16:38:55 +0800

    0703提交

--- app/src/main/java/com/example/wifi/ui/login/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/example/wifi/ui/login/ui/gallery/GalleryFragment.java
@@ -4,32 +4,51 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.wifi.R;
 
-public class GalleryFragment extends Fragment {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class GalleryFragment extends Fragment  {
 
     private GalleryViewModel galleryViewModel;
+    private List<Map<String, Object>> lists;
+    private SimpleAdapter adapter;
+    private ListView listView;
+
+    private String[] theme = {"鼻涕怪", "臭脚怪", "便便怪","呕吐怪"};
+    private String[] content1 = {"战斗力 5", "战斗力 6", "战斗力 7", "战斗力 7"};
+    private int[] imageViews = {R.drawable.mon1, R.drawable.mon2, R.drawable.mon3, R.drawable.mon4};
+    private String[] data = {"稀有度3","稀有度2","稀有度4","稀有度5"};
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         galleryViewModel =
                 ViewModelProviders.of(this).get(GalleryViewModel.class);
         View root = inflater.inflate(R.layout.fragment_gallery, container, false);
-        final TextView textView = root.findViewById(R.id.text_gallery);
-        galleryViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        lists = new ArrayList<>();
+        for (int i = 0; i < theme.length; i++) {
+            Map<String, Object> map = new HashMap<>();
+            map.put("image", imageViews[i]);
+            map.put("theme", theme[i]);
+            map.put("content", content1[i]);
+            map.put("data",data[i]);
+            lists.add(map);
+        }
+
+
+        adapter = new SimpleAdapter(getActivity(), lists, R.layout.monsterunit, new String[]{"image", "theme", "content","data"}, new int[]{R.id.image1, R.id.text1, R.id.text2, R.id.text3});
+        listView = (ListView) root.findViewById(R.id.listview);
+        listView.setAdapter(adapter);
         return root;
     }
 }
--- app/src/main/java/com/example/wifi/ui/login/ui/slideshow/SlideshowFragment.java
+++ app/src/main/java/com/example/wifi/ui/login/ui/slideshow/SlideshowFragment.java
@@ -4,32 +4,51 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.wifi.R;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class SlideshowFragment extends Fragment {
 
     private SlideshowViewModel slideshowViewModel;
+    private List<Map<String, Object>> lists;
+    private SimpleAdapter adapter;
+    private ListView listView;
+
+    private String[] theme = {"张明", "李明", "李明"};
+    private String[] content = {"600 602 501", "666 620 502", "666 620 503"};
+    private int[] imageViews = {R.drawable.icon1, R.drawable.icon2, R.drawable.icon3};
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         slideshowViewModel =
                 ViewModelProviders.of(this).get(SlideshowViewModel.class);
         View root = inflater.inflate(R.layout.fragment_slideshow, container, false);
-        final TextView textView = root.findViewById(R.id.text_slideshow);
-        slideshowViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        lists = new ArrayList<>();
+        for (int i = 0; i < theme.length; i++) {
+            Map<String, Object> map = new HashMap<>();
+            map.put("image", imageViews[i]);
+            map.put("theme", theme[i]);
+            map.put("content", content[i]);
+            lists.add(map);
+        }
+
+
+        adapter = new SimpleAdapter(getActivity(), lists, R.layout.unit, new String[]{"image", "theme", "content"}, new int[]{R.id.image1, R.id.text1, R.id.text2});
+        listView = (ListView) root.findViewById(R.id.listview2);
+        listView.setAdapter(adapter);
+
+
         return root;
     }
 }
